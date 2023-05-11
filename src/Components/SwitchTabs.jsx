5ceee6932d56a0169a15ac36a8c5b21f7d4ba793
vinaysingh-chaudhary <vinay.heyworld@gmail.com>
2023-05-12 00:13:55 +0530

    SwitchTab creation for home page section's time period

--- src/Components/SwitchTabs.jsx
+++ src/Components/SwitchTabs.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
 const SwitchTabs = ({data, onTabFunction}) => {
+
+
   return (
     <div className='w-[100%] h-[100%] flex justify-around items-center'>
-        {data.map((element, index) => {
-            return <button className='focus:border-2 focus:border-black' onClick={onTabFunction(index)}>{element}</button>
+        {data.map((tab, index) => {
+            return <button className='focus:border-2 focus:border-black' key={index} onClick={() => onTabFunction(tab, index)}>
+                {tab}
+                </button>
         })}
     </div>
   )
--- src/Pages/homePage/TrendingCarousel.jsx
+++ src/Pages/homePage/TrendingCarousel.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useFetch from '../../Hooks/useFetch'
 import SwitchTabs from '../../Components/SwitchTabs'
 
 const TrendingCarousel = () => {
 
+    const [time_window, setTime_Window] = useState("week")
+    // console.log(time_window);
+
+    const trending = useFetch(`/trending/all/${time_window}`)
+    // console.log(trending);
 
 
-    const trending = useFetch("/trending/all/week")
-    console.log(trending);
+    const onTabFunction = (tab, index) => {
+        if(index===0){
 
-    const onTabFunction = (tab) => {
-        if(tab===tab[0]){
-            console.log("Day")
+            setTime_Window(tab);
         }else{
-            console.log("week")
+            console.log(tab);
+            setTime_Window(tab);
         }
     }
 
@@ -23,7 +27,7 @@ const TrendingCarousel = () => {
             <h1>Trending</h1>
 
             <div className=' w-[20%]  border-pink-500 border-2'>
-                <SwitchTabs data={["Day", "Week"]} onTabFunction={onTabFunction}/>
+                <SwitchTabs data={["day", "week"]} onTabFunction={onTabFunction}/>
             </div>
         </div>
 
