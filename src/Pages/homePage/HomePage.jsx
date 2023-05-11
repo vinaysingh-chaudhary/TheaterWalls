256c756129e91effc08823dedc0f53a0d2330c63
vinaysingh-chaudhary <vinay.heyworld@gmail.com>
2023-05-11 23:49:20 +0530

    Modifications in Home page and Navbar

--- src/Pages/homePage/HomePage.jsx
+++ src/Pages/homePage/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import HeroBanner from '../../Components/HeroBanner'
+import TrendingCarousel from '../homePage/TrendingCarousel'
 
 
 const HomePage = () => {
   return (
     <div>
       <HeroBanner/>
+      <TrendingCarousel/>
+
     </div>
   )
 }
